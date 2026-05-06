99debc85520ce4c490e61477f4f9d3fa217cb30e
Sofie Hedvig Grøndal Jakobsen <sofijakobsen@gmail.com>
2026-05-06 10:46:20 +0200

    en read all function som kan læse specefike filer og selv bestemme antallet af linjer. der er også i function.R skrevet ind i funktionen så man nemt kan ændre pathwayen.

--- R/functions.R
+++ R/functions.R
@@ -14,3 +14,35 @@ read <- function(file_path, max_rows = 100) {
   return(data)
 }
 
+
+
+
+
+
+# Global variables -----
+.DATASET_DIR <- here::here("data-raw/nurses-stress/") # 1
+
+#' Read in all files in the nurse-stress folder
+#'
+#' @param file the name or type of data that needs to be loaded.
+#'
+#' @returns Dataframe with a Id, collectioin time and the data point ef hr.
+
+read_all <- function(file, max_rows = 100) {
+  # Code that does something
+  files <- .DATASET_DIR |> #pipe the first argument
+    fs::dir_ls(regexp = file,
+               recurse = TRUE)
+
+  data <- files |>
+    purrr::map(\(file) read(file, max_rows = max_rows)) |> # er ikke sikker på jeg forstår denne del
+    purrr::list_rbind(names_to = "files_path_id")
+
+  return(data)
+}
+
+# this function was moved to functions.R
+
+
+
+
